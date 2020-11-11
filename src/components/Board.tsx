13fd9e7998f58d8743c4e112b6c1451a71c6b72d
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-11-11 22:17:44 +0900

    board name

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "app.css";
 
 import Typography from "@material-ui/core/Typography";
@@ -14,21 +14,38 @@ const useStyles = makeStyles((theme) => ({
     top: "58px",
     left: "1vw",
   },
-  boardName: {},
+  boardName: {
+    display: "flex",
+  },
   boardNameText: {
+    padding: "2px 20px",
+    flex: "0 1 auto",
     color: "white",
+    borderRadius: "5px",
     fontFamily: `"Jua", sans-serif`,
     cursor: "pointer",
+    fontSize: "18px",
     "&:hover": {
       backgroundColor: "rgba(154,160,163, 0.9)",
     },
   },
+  boardNameForm: {},
   boardNameInput: {
+    padding: "0 20px",
     outline: "none",
     border: "none",
     borderRadius: "5px",
-    height: "30px",
+    height: "34px",
     fontSize: "18px",
+  },
+  boardNameWidth: {
+    fontSize: "18px",
+    color: "rgba(0,0,0,0)",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    userSelect: "none",
+    zIndex: -1,
   },
 }));
 
@@ -39,13 +56,22 @@ const Board = () => {
   const [text, setText] = useState("");
   const [textInput, setTextInput] = useState("invisible");
 
+  const bnEl = useRef<HTMLInputElement>(null);
+  const wtEl = useRef<HTMLInputElement>(null);
+
   const onClickText = () => {
     setText("invisible");
     setTextInput("");
+    if (bnEl.current && wtEl.current) {
+      bnEl.current.style.width = wtEl.current.scrollWidth + 4 + "px";
+    }
   };
   const onBlurInput = () => {
     setText("");
     setTextInput("invisible");
+    if (boardName === "") {
+      setBoardName("My Board");
+    }
   };
   const onSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -55,6 +81,10 @@ const Board = () => {
   const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setBoardName(value);
+
+    if (bnEl.current && wtEl.current) {
+      bnEl.current.style.width = wtEl.current.scrollWidth + 24 + "px";
+    }
   };
 
   return (
@@ -69,15 +99,23 @@ const Board = () => {
           >
             {boardName}
           </Typography>
-          <form id={textInput} onSubmit={onSubmitForm}>
+          <form
+            className={classes.boardNameForm}
+            id={textInput}
+            onSubmit={onSubmitForm}
+          >
             <input
+              ref={bnEl}
               className={classes.boardNameInput}
               value={boardName}
               onChange={onChangeName}
-              maxLength={10}
+              required
               onBlur={onBlurInput}
             />
           </form>
+          <span ref={wtEl} className={classes.boardNameWidth}>
+            {boardName}
+          </span>
         </div>
       </div>
     </>
