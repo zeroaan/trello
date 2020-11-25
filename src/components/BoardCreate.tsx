281f5c7259d2ad8e8bbed05a07ed4876994f354f
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-11-25 12:29:31 +0900

    Update BoardCreate styled-component

--- src/components/BoardCreate.tsx
+++ src/components/BoardCreate.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, Dispatch, SetStateAction } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router";
+import styled from "styled-components";
 
 import { addBoard } from "store/actions/trello";
 import { RootState } from "store/reducers";
@@ -9,79 +10,74 @@ import { BoardState } from "store/reducers/trello";
 
 import Card from "@material-ui/core/Card";
 import CloseIcon from "@material-ui/icons/Close";
-import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
-  newBoard: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    width: "100vw",
-    height: "100vh",
-    zIndex: 1100,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-  addBoard: {
-    backgroundColor: "rgb(235,236,240)",
-    width: "370px",
-    height: "200px",
-    margin: "auto",
-    position: "relative",
-    top: "230px",
-  },
-  addBoardtitle: {
-    position: "relative",
-    top: "25px",
-    left: "43px",
-  },
-  addBoardInput: {
-    position: "relative",
-    top: "50px",
-    left: "40px",
-    fontSize: "15px",
-    outline: "none",
-    border: "2px solid rgb(0,121,191)",
-    borderRadius: "5px",
-    width: "256px",
-    height: "17px",
-    padding: "10px 10px",
-    resize: "none",
-    "&::-webkit-scrollbar": {
-      display: "none",
-    },
-  },
-  addBoardBt: {
-    position: "relative",
-    top: "60px",
-    left: "115px",
-    outline: "none",
-    border: "none",
-    zIndex: 1200,
-    borderRadius: "5px",
-    backgroundColor: "rgb(90,172,68)",
-    marginTop: "10px",
-    color: "white",
-    padding: "8px 16px",
-    fontSize: "15px",
-    cursor: "pointer",
-  },
-  closeIcon: {
-    position: "relative",
-    top: "-50px",
-    left: "180px",
-    cursor: "pointer",
-    color: "rgb(108,120,141)",
-    fontSize: "28px",
-  },
-});
+const DivNewBoard = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 1100;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+const CardStyled = styled(Card)`
+  background-color: rgb(235, 236, 240);
+  width: 370px;
+  height: 200px;
+  margin: auto;
+  position: relative;
+  top: 230px;
+`;
+const H3BoardTitle = styled.h3`
+  position: relative;
+  top: 25px;
+  left: 43px;
+`;
+const InputStyled = styled.input`
+  position: relative;
+  top: 50px;
+  left: 40px;
+  font-size: 15px;
+  outline: none;
+  border: 2px solid rgb(0, 121, 191);
+  border-radius: 5px;
+  width: 256px;
+  height: 17px;
+  padding: 10px 10px;
+  resize: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+const ButtonStyled = styled.button`
+  position: relative;
+  top: 60px;
+  left: 115px;
+  outline: none;
+  border: none;
+  z-index: 1200;
+  border-radius: 5px;
+  background-color: rgb(90, 172, 68);
+  margin-top: 10px;
+  color: white;
+  padding: 8px 16px;
+  font-size: 15px;
+  cursor: pointer;
+`;
+const CloseIconStyled = styled(CloseIcon)`
+  position: relative;
+  top: -50px;
+  left: 180px;
+  cursor: pointer;
+  color: rgb(108, 120, 141);
+  font-size: 28px;
+`;
 
 interface Props {
   setCreateBoard: Dispatch<SetStateAction<boolean>>;
 }
 
 const BoardCreate: React.FC<Props> = ({ setCreateBoard }) => {
-  const classes = useStyles();
-
   const history = useHistory();
 
   const dispatch = useDispatch();
@@ -108,23 +104,20 @@ const BoardCreate: React.FC<Props> = ({ setCreateBoard }) => {
 
   return (
     <>
-      <div className={classes.newBoard}>
-        <Card className={classes.addBoard}>
-          <h3 className={classes.addBoardtitle}>New Board</h3>
-          <input
-            className={classes.addBoardInput}
+      <DivNewBoard>
+        <CardStyled>
+          <H3BoardTitle>New Board</H3BoardTitle>
+          <InputStyled
             placeholder="Input Board Name ..."
-            onChange={onChangeBoard}
             value={newBoard}
+            onChange={onChangeBoard}
             maxLength={15}
             autoFocus
           />
-          <button className={classes.addBoardBt} onClick={onClickAddBoard}>
-            Create a Board
-          </button>
-          <CloseIcon className={classes.closeIcon} onClick={onClickClose} />
-        </Card>
-      </div>
+          <ButtonStyled onClick={onClickAddBoard}>Create a Board</ButtonStyled>
+          <CloseIconStyled onClick={onClickClose} />
+        </CardStyled>
+      </DivNewBoard>
     </>
   );
 };
